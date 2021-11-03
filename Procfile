d3d4087a9c06474c7e534c7130c43a350390c6e7
scode24 <sarkar.soumyabrata2@gmail.com>
2021-11-03 22:09:24 +0530

    FIX : Failed to find application object ‘server’ in ‘app’

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn apiService:server
+web: gunicorn apiService:app
--- apiService.py
+++ apiService.py
@@ -2,8 +2,10 @@ from flask import Flask, request
 import pandas as pd
 from sklearn import svm
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/predict', methods=['POST', 'GET'])
